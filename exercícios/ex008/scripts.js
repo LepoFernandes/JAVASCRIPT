e94aa6ca5fedb96b172b084c211d02f2bf81e6db
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-10-24 12:13:06 -0300

    avançando js

---
+++ exercícios/ex008/scripts.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+    console.log(`Dirija com mais cuidado!`)
+}
+console.log(`Boa viagem!`)
