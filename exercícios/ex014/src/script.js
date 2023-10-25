9697ef82a6b099f235a68d452320fd1d8d2f2eb7
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-10-25 17:56:24 -0300

    exercicios novos

---
+++ exercícios/ex014/src/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.` 
+   
+    if(hora >= 0 && hora < 12){
+        img.src = 'src/manhã.jpg'
+        document.body.style.background = '#FFD4B2'
+    } else if(hora >= 12 && hora <= 18){
+        img.src = 'src/tarde.jpg'
+        document.body.style.background = '#4D2A3A'
+    } else {
+        img.src = 'src/noite.jpg'
+        document.body.style.background = '#392C47'
+    }
+}
