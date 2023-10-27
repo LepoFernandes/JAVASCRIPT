81a0604e9e94010cff8727c0692bdf4f82068881
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-10-27 15:18:42 -0300

    fim

--- exercícios/ex015/script.js
+++ exercícios/ex015/script.js
@@ -4,10 +4,50 @@ function verificar(){
     var fano = window.document.getElementById('txtano')
     var res = window.document.getElementById('res')
 
-    if(fano.ariaValueMax.length == 0 || fano.value > ano){
+    if(fano.value.length == 0 || fano.value > ano){
         window.alert('Erro, verifique os dados e envie novamente!')
     } else {
-        window.alert('Tudo ok!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade <10){
+                //criança
+                img.setAttribute('src', './src/homem-criança.jpg')
+            } else if ( idade < 30){
+                //jovem
+                img.setAttribute('src', './src/homem-jovem.jpg')
+            } else if ( idade < 50){
+                //adulto
+                img.setAttribute('src', './src/homem-adulto.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', './src/homem-idoso.jpg')
+            }
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade <10){
+                //criança
+                img.setAttribute('src', './src/mulher-criança.jpg')
+            } else if ( idade < 30){
+                //jovem
+                img.setAttribute('src', './src/mulher-jovem.jpg')
+            } else if ( idade < 50){
+                //adulto  
+                img.setAttribute('src', './src/mulher-adulta.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', './src/mulher-idosa.jpg')
+            }
+        }
+
+        res.style.textAlign = 'Center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
 
