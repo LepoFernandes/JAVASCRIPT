5597e44f761e537ee1ea43218307b4683d7c07dd
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-10-30 10:12:41 -0300

    Create ambiente.js

---
+++ exercícios/ex016/ambiente.js
@@ -0,0 +1,23 @@
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6) 
+
+
+/* MANEIRA WHILE 
+var c = 1
+while( c <= 6 ){
+    console.log(`Passo ${c}`)
+    c++  
+} */
+
+
+/* MANEIRA CONVENCIONAL
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)*/
