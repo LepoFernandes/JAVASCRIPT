65868e9cbbd69650715b0e0c3e550e7a402438a3
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-11-01 11:25:31 -0300

    ex novos

---
+++ exercícios/ex022/function01.js
@@ -0,0 +1,9 @@
+var res = parimpar(8)
+function parimpar(n){
+    if(n%2==0){
+        return 'O número é par!'
+    } else {
+        return 'O número é ímpar!'
+    }
+}
+console.log(res)
---
+++ exercícios/ex022/function04.js
@@ -0,0 +1,9 @@
+// 5! = 5 x 4 x 3 x 2 x 1
+
+
+function fatorial(n){
+    var fat = 1
+    for(var c= n; c > 1; c--){
+        fat *= c 
+    } return fat
+} console.log(fatorial(5))
