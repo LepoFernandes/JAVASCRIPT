9ce90e0930da28dd462a15766c4e08bba2d43329
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-11-02 23:49:21 -0300

    Update script.js

--- exercícios/ex023/script.js
+++ exercícios/ex023/script.js
@@ -20,15 +20,40 @@ function enviar(){
    }
 }   
 function finalizar(){ 
-   
-   
-   
-   
-   
-   var div = window.document.getElementById('res')
-   div.innerHTML = `Ao todo, temos ${n.length} elementos cadastrados.`
-
+   var div = window.document.getElementById('res') 
+   if (n.length === 0) {
+      div.innerHTML = 'Nenhum valor cadastrado.';
+  } else {
+      // Inicialize as variáveis para o maior e menor valores
+      var maiorValor = n[0];
+      var menorValor = n[0];
+      var somaValores = 0;
+      // Encontre o maior e o menor valor e calcule a soma
+      for (var i = 0; i < n.length; i++) {
+          if (n[i] > maiorValor) {
+              maiorValor = n[i];
+          }
+          if (n[i] < menorValor) {
+              menorValor = n[i];
+          }
+          somaValores += n[i];
+      }
+      // Calcule a média dos valores
+      var mediaValores = somaValores / n.length;
+      // Exiba as informações na div 'res'
+      div.innerHTML = `Ao todo, temos ${n.length} elementos cadastrados.<br>`;
+      div.innerHTML += `O maior valor é: ${maiorValor}<br>`;
+      div.innerHTML += `O menor valor é: ${menorValor}<br>`;
+      div.innerHTML += `A soma dos valores é: ${somaValores}<br>`;
+      div.innerHTML += `A média dos valores é: ${mediaValores.toFixed(2)}`;
+  }
 }
+   
+   
+   
+   
+   
+
 
 
 
